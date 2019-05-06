30fb0ac458002729f613b72b2aa796ef8fdda712
root <root@ip-172-31-17-194.us-west-2.compute.internal>
2019-05-06 21:51:00 +0000

    Init

---
+++ plugins/realm/addressbook/Plugin.php
@@ -0,0 +1,96 @@
+<?php namespace Realm\AddressBook;
+
+use Backend;
+use System\Classes\PluginBase;
+
+/**
+ * AddressBook Plugin Information File
+ */
+class Plugin extends PluginBase
+{
+    /**
+     * Returns information about this plugin.
+     *
+     * @return array
+     */
+    public function pluginDetails()
+    {
+        return [
+            'name'        => 'AddressBook',
+            'description' => 'No description provided yet...',
+            'author'      => 'Realm',
+            'icon'        => 'icon-leaf'
+        ];
+    }
+
+    /**
+     * Register method, called when the plugin is first registered.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+
+    /**
+     * Boot method, called right before the request route.
+     *
+     * @return array
+     */
+    public function boot()
+    {
+
+    }
+
+    /**
+     * Registers any front-end components implemented in this plugin.
+     *
+     * @return array
+     */
+    public function registerComponents()
+    {
+        return []; // Remove this line to activate
+
+        return [
+            'Realm\AddressBook\Components\MyComponent' => 'myComponent',
+        ];
+    }
+
+    /**
+     * Registers any back-end permissions used by this plugin.
+     *
+     * @return array
+     */
+    public function registerPermissions()
+    {
+        return []; // Remove this line to activate
+
+        return [
+            'realm.addressbook.some_permission' => [
+                'tab' => 'AddressBook',
+                'label' => 'Some permission'
+            ],
+        ];
+    }
+
+    /**
+     * Registers back-end navigation items for this plugin.
+     *
+     * @return array
+     */
+    public function registerNavigation()
+    {
+        return []; // Remove this line to activate
+
+        return [
+            'addressbook' => [
+                'label'       => 'AddressBook',
+                'url'         => Backend::url('realm/addressbook/mycontroller'),
+                'icon'        => 'icon-leaf',
+                'permissions' => ['realm.addressbook.*'],
+                'order'       => 500,
+            ],
+        ];
+    }
+}
